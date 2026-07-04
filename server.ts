82de7ba3680eeb2125eb5bb2b6c73a3346a0fbe8
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-07-04 17:26:14 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import path from "path";
-import { createServer as createViteServer } from "vite";
 import dns from "dns";
 
 // For potential DNS issues in sandboxes
